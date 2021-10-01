683c0465fe72f5f44a37512f7581efc0878420cf
lokal-profil <lokal.profil@gmail.com>
2021-10-01 12:23:47 +0200

    Rokkad

--- harvest.py
+++ harvest.py
@@ -2,100 +2,30 @@
 Harvest items needing librisxl id.
 
 """
-from bs4 import BeautifulSoup
 import requests
-# import isfdb
+from bs4 import BeautifulSoup
 
-HOST = 'http://www.isfdb.org'
+from isfdb import IsfdbSession
+
+LIBRIS_IDTYPE = 30
+LIBRISXL_IDTYPE = 31
 MISSING_LIBRISXL_REPORT = 300
 
 # rename file something with libris
 
 
-# move to isfdb.py - replaced by get_xml_data_by_record_id()
-def _get_record_page(record_id):
-    url = '{0}/cgi-bin/pl.cgi?{1}'.format(HOST, record_id)
-    r = requests.get(url)
-    return r.content
-
-
-# largely replaced by replaced by get_xml_data_by_record_id()
-def find_libris_id(record_id):
-    ext_ids = None
-    soup = BeautifulSoup(_get_record_page(record_id), features='html5lib')
-    # must limit search to external ID field to not pick up abbr tags in notes
-    for entry in soup.find_all('b'):
-        if entry.get_text() == 'External IDs:':
-            ext_ids = entry.parent
-    libris_links = ext_ids.find_all(
-        'abbr', {'title': 'Libris - National Library of Sweden'})
-    if len(libris_links) != 1:
-        raise Exception(
-            '{0} contained {1} libris entries which is not supported'.format(
-                record_id, len(libris_links)))
-    libris_id = libris_links[0].next_sibling.strip(': ')
-    return libris_id
-
-
-# move to isfdb
-def _get_cleanup_report(report_id):
-    browser = log_in()
-    url = '{0}/cgi-bin/edit/cleanup_report.cgi?{1}'.format(HOST, report_id)
-    browser.get(url)
-    return browser.page_source
-
-
-# move to isfdb
-# load username and password from credentials but allow getting password from
-# `from getpass import getpass \n password = getpass()`
-# to run headless (if debug=False) see https://stackoverflow.com/questions/46753393
-# Note that you may need browser.implicitly_wait(XSECONDS)
-def log_in(username=None, password=None):
-    # this should set some sort of flag about us being logged in
-    if not username:
-        username = input('Username: ')
-    if not password:
-        from getpass import getpass
-        password = getpass()
-    from selenium import webdriver
-    # required geckodriver
-    browser = webdriver.Firefox()
-    # load login page
-    url = '{0}/cgi-bin/dologin.cgi?dologin.cgi+0'.format(HOST)
-    browser.get(url)
-    browser.find_element_by_name('login').send_keys(username)
-    browser.find_element_by_name('password').send_keys(password)
-    browser.find_element_by_xpath("//input[@value='submit']").click()
-    # wait for login to complete
-    browser.implicitly_wait(3)
-    result = (
-        browser.find_element_by_id('statusbar')
-        .find_element_by_tag_name('h2')
-        .text)
-    if result.lower().startswith('login failed'):
-        msg = (
-            browser.find_element_by_id('main2')
-            .find_element_by_tag_name('h2')
-            .text)
-        browser.quit()
-        raise Exception(msg)  # @todo: find more appropriate exception
-    else:
-        return browser
-
-
 # make generic by returning [(cols), ] or OrderedList({col_label: col_value})?
 # would then have to run soup on each row though
 # For this one return [ (id, name), ]
-def harvest_records_from_cleanup_report(max=5):
+def harvest_records_from_cleanup_report(isfdb, max=5):
     count = 0
     records = []
     soup = BeautifulSoup(
-        _get_cleanup_report(MISSING_LIBRISXL_REPORT),
+        isfdb.get_cleanup_report(MISSING_LIBRISXL_REPORT),
         features='html5lib')
     for link in soup.find(id='main2').find_all('a'):
         record_id = link.get('href').split('?')[1]
-        libris_id = find_libris_id(record_id)
-        records.append((libris_id, link.text))
+        records.append((record_id, link.text))
         count += 1
         if count >= max:
             break
@@ -106,10 +36,63 @@ def harvest_records_from_cleanup_report(max=5):
     return records
 
 
+def get_librisxl_id(libris_id):
+    clearing_url = 'https://libris.kb.se/resource/bib/{0}'.format(libris_id)
+    r = requests.head(clearing_url, allow_redirects=True)
+    resolved_url = r.url
+    if r.status_code != 500:
+        raise ValueError('Unable to get LibrisXL id')
+    return resolved_url.rpartition('/')[2].partition('#')[0]
+
+
+# @todo: rename
+def add_librisxl_id(isfdb, record_id):
+    """
+    Structure of external-id part is:
+    External_IDs
+       External_ID
+          IDtype
+          IDvalue
+    """
+    mod_note = 'Adding LibrisXL ID based on Libris ID (by api)'
+    # get entry to ensure it exists and to get remaining external-ids
+    record_result = isfdb.get_pub_data_by_record_id(record_id)
+    old_data = record_result['ISFDB']['Publications']['Publication']
+
+    # extract all pre-existing external ids
+    # extract pre existing identifiers and check for edge cases
+    # not that submission format does not include the IDtypeName key
+    external_id = []
+    libris_id = None
+    for entry in old_data.get('External_IDs').get('External_ID'):
+        if int(entry.get('IDtype')) == LIBRISXL_IDTYPE:
+            raise ValueError('Publication already has LibrisXL ID.')
+        elif int(entry.get('IDtype')) == LIBRIS_IDTYPE:
+            if libris_id:
+                raise ValueError('Publication has multiple Libris IDs.')
+            libris_id = entry.get('IDvalue')
+        external_id.append({
+            'IDtype': entry.get('IDtype'),
+            'IDvalue': entry.get('IDvalue')})
+
+    # add new id
+    librisxl_id = get_librisxl_id(libris_id)
+    external_id.append({
+        'IDtype': LIBRISXL_IDTYPE,
+        'IDvalue': librisxl_id})
+
+    # package as update
+    update = {'External_IDs': {'External_ID': external_id}}
+    return isfdb.update_publication(old_data, update, mod_note)
+
+
 # drop
 def test():
-    # find_libris_id(669460)
-    harvest_records_from_cleanup_report()
+    isfdb = IsfdbSession()
+    print('===Harvest records===')
+    #harvest_records_from_cleanup_report(isfdb)
+    print('===Update an entry new===')
+    add_librisxl_id(isfdb, 645691)
 
 
 # drop
--- isfdb.py
+++ isfdb.py
@@ -4,182 +4,201 @@ Small script for interacting with isfdb.org.
 Based on http://www.isfdb.org/wiki/index.php/Web_API
 """
 import json
+from getpass import getpass
+from xml.parsers.expat import ExpatError as XmlParseError
 
 import requests
 import xmltodict
-from xml.parsers.expat import ExpatError as XmlParseError
-
+from selenium import webdriver
 
 HOST = 'http://www.isfdb.org'
 HEADERS = {
     'User-Agent': 'Lokal_Profil ISFDB test script 0.1'
 }
-CREDENTIALS = {}
 
-# @TODO: Set up a class IsfdSession which takes the user agent (or parts
-# thereof), max submissions = 20, stores browser (to avoid re-login), sets dry
-# stores credentials...
+
+# Add max_submissions = 20 and make_submission should respect this
 # Add a get_pending_edits_number
-# Add mechanism for closing session and killing browser
+# Add mechanism for closing session and killing browser (see commented out)
+# Make browser headless (on dry=False)
+class IsfdbSession(object):
+    """An isfdb.org session be it via API or browser."""
 
+    # @todo: proper docstring
+    def __init__(self, headers=None, dry=True):
+        """
+        ...
 
-# if adding optional password then validation needs to change as does passing
-# credentials straight to make_submission
-# can then use more sane credentials labels
-def _load_credentials():
-    global CREDENTIALS
-    if not CREDENTIALS:
+        @param headers: request headers
+        @param dry: if session should be run in dry/debug mode. In this mode
+            no submissions are made.
+        """
+        self._browser = None
+        self._credentials = None
+        self.headers = headers or HEADERS
+        self.dry = dry
+
+    # def __enter__(self):
+    #     return self
+    #
+    # def __exit__(self):
+    #     """Ensure no (headless) browsers are left after use."""
+    #     if self._browser:
+    #         self._browser.quit()
+
+    @property
+    def credentials(self):
+        """Submission credentials for Web API."""
+        if not self._credentials:
+            self._credentials = IsfdbSession._load_credentials()
+        return self._credentials
+
+    # if adding web password then validation needs to change as does passing
+    # credentials straight to make_submission
+    # can then use more sane credentials labels
+    @staticmethod
+    def _load_credentials():
+        """Load API credentials from .credentials.json file."""
         with open('.credentials.json') as f:
             data = json.load(f)
             if not set(data.keys()) == set(['Submitter', 'LicenseKey']):
                 raise ValueError('Incorrectly formatted credentials file')
-            CREDENTIALS = data
-    return CREDENTIALS
-
-
-def get_pub_data_by_extid(extid_type, extid):
-    """."""
-    url = '{0}/cgi-bin/rest/getpub_by_ID.cgi?{1}+{2}'.format(
-        HOST, extid_type, extid)
-    r = requests.get(url, headers=HEADERS)
-    return validate_and_parse_xml_response(r)
-
-
-def get_pub_data_by_record_id(record_id):
-    """."""
-    url = '{0}/cgi-bin/rest/getpub_by_internal_ID.cgi?{1}'.format(
-        HOST, record_id)
-    r = requests.get(url, headers=HEADERS)
-    return validate_and_parse_xml_response(r)
-
-
-def validate_and_parse_xml_response(request):
-    """Check if response is xml, if so parse, else raise error."""
-    try:
-        data = xmltodict.parse(request.content)
-    except XmlParseError:
-        raise Exception(request.content.strip())  # @todo find better exception
-    else:
         return data
 
+    @property
+    def browser(self):
+        """Browser logged into isfdb.org providing pages not in API."""
+        if not self._browser:
+            self._browser = self._log_in()
+        return self._browser
 
-# add holder support - class variable
-def make_submission(submission_type, data, subject, mod_note, dry=True):
-    """
-    data is dict, subject is string, submissionType is string.
-    subject might have a default value
-    Add global counter to max out at 20
-    """
-    payload = {
-        'IsfdbSubmission': {
-            submission_type: {
-                'Subject': subject,
-                'ModNote': mod_note,
-                ** data,
-                **_load_credentials()
+    # to run headless (if debug=False) see
+    # https://stackoverflow.com/questions/46753393
+    def _log_in(self):
+        """
+        Create a selenium browser logged in to isfdb.org.
+
+        This currently requires geckodriver to be installed and will open a
+        separate browser window.
+        """
+        username = self.credentials.get('Submitter')
+        password = self.credentials.get('password')
+        if not username:
+            username = input('Username: ')
+        if not password:
+            password = getpass()
+        # required geckodriver
+        browser = webdriver.Firefox()
+        # load login page
+        url = '{0}/cgi-bin/dologin.cgi?0+0'.format(HOST)
+        browser.get(url)
+        browser.find_element_by_name('login').send_keys(username)
+        browser.find_element_by_name('password').send_keys(password)
+        browser.find_element_by_xpath("//input[@value='submit']").click()
+        # wait for login to complete
+        browser.implicitly_wait(3)
+        result = (
+            browser.find_element_by_id('statusbar')
+            .find_element_by_tag_name('h2')
+            .text)
+        if result.lower().startswith('login failed'):
+            msg = (
+                browser.find_element_by_id('main2')
+                .find_element_by_tag_name('h2')
+                .text)
+            browser.quit()
+            raise ConnectionError(msg)  # @todo: more appropriate exception?
+        else:
+            return browser
+
+    def get_pub_data_by_external_id(self, extid_type, ext_id):
+        """
+        Get all publication records matching a provided external identifier.
+
+        For external id type names see:
+        http://www.isfdb.org/cgi-bin/adv_identifier_search.cgi
+        """
+        url = '{0}/cgi-bin/rest/getpub_by_ID.cgi?{1}+{2}'.format(
+            HOST, extid_type, ext_id)
+        r = requests.get(url, headers=self.headers)
+        return IsfdbSession._validate_and_parse_xml_response(r)
+
+    def get_pub_data_by_record_id(self, record_id):
+        """Get the publication record matching an internal isfdb identifier."""
+        url = '{0}/cgi-bin/rest/getpub_by_internal_ID.cgi?{1}'.format(
+            HOST, record_id)
+        r = requests.get(url, headers=self.headers)
+        return IsfdbSession._validate_and_parse_xml_response(r)
+
+    @staticmethod
+    def _validate_and_parse_xml_response(request):
+        """Check if response is xml, if so parse, else raise error."""
+        try:
+            data = xmltodict.parse(request.content)
+        except XmlParseError:
+            raise ValueError(request.content.strip())  # @todo right exception?
+        else:
+            return data
+
+    def get_cleanup_report(self, report_id):
+        """Return the html contents of a cleanup report."""
+        url = '{0}/cgi-bin/edit/cleanup_report.cgi?{1}'.format(HOST, report_id)
+        self.browser.get(url)
+        return self.browser.page_source
+
+    # @todo: add holder support - class variable
+    # @todo: add proper docstring
+    def make_submission(self, submission_type, data, subject, mod_note):
+        """
+        data is dict, subject is string, submissionType is string.
+        subject might have a default value
+        Add global counter to max out at 20
+        """
+        payload = {
+            'IsfdbSubmission': {
+                submission_type: {
+                    'Subject': subject,
+                    'ModNote': mod_note,
+                    **data,
+                    **self.credentials
+                }
             }
         }
-    }
-    url = '{0}/cgi-bin/rest/submission.cgi'.format(HOST)
-    if dry:
-        print(xml_encode(payload))
-    else:
-        r = requests.post(url, data=xml_encode(payload), headers=HEADERS)
-        parse_submission_result(r)
-        return r
-
-
-def parse_submission_result(r):
-    """Check submission result to flag any failure"""
-    result = validate_and_parse_xml_response(r)
-    status = result.get('ISFDB').get('Status')
-    if status == 'FAIL':
-        raise ConnectionError(result.get('ISFDB').get('Error'))
-
-
-def xml_encode(payload):
-    """Encode dict as xml with the appropriate encoding."""
-    return xmltodict.unparse(payload, encoding='iso-8859-1', pretty=True)
-
-
-# add holder support
-def update_publication(old_data, update, mod_note):
-    """
-    Can make this only support additions for now?
-    Expect data to be a dict. build xml_payload later
-
-    http://www.isfdb.org/wiki/index.php/XML:PubUpdate
-    """
-    data = {
-        'Record': old_data['Record'],
-        **update
-    }
-    return make_submission('PubUpdate', data, old_data['Title'], mod_note)
-
-
-# less generic from here
-LIBRISXL_IDTYPE = 31
-
-
-def get_libris_data(libris_id):
-    """."""
-    d = get_pub_data_by_extid('Libris', libris_id)
-    if int(d['ISFDB']['Records']) != 1:
-        if int(d['ISFDB']['Records']) > 1:
-            raise ValueError('Non-unique Libris match')
+        url = '{0}/cgi-bin/rest/submission.cgi'.format(HOST)
+        if self.dry:
+            print(IsfdbSession.xml_encode(payload))
         else:
-            raise ValueError('No matching Libris entry')
-    return d['ISFDB']['Publications']['Publication']
+            r = requests.post(
+                url,
+                data=IsfdbSession.xml_encode(payload),
+                headers=self.headers)
+            self._parse_submission_result(r)
+            return r
 
+    def _parse_submission_result(self, request):
+        """Check submission result to flag any failure"""
+        result = IsfdbSession.validate_and_parse_xml_response(request)
+        status = result.get('ISFDB').get('Status')
+        if status == 'FAIL':
+            raise ConnectionError(result.get('ISFDB').get('Error'))
 
-def get_librisxl_id(libris_id):
-    clearing_url = 'https://libris.kb.se/resource/bib/{0}'.format(libris_id)
-    r = requests.head(clearing_url, allow_redirects=True)
-    resolved_url = r.url
-    if r.status_code != 500:
-        raise ValueError('Unable to get LibrisXL id')
-    return resolved_url.rpartition('/')[2].partition('#')[0]
+    @staticmethod
+    def xml_encode(payload):
+        """Encode dict as xml with the appropriate encoding."""
+        return xmltodict.unparse(payload, encoding='iso-8859-1', pretty=True)
 
+    # @todo: add holder support
+    # @todo: proper docstring
+    def update_publication(self, old_data, update, mod_note):
+        """
+        Can make this only support additions for now?
+        Expect data to be a dict. build xml_payload later
 
-def add_librisxl_id(libris_id):
-    """
-    Structure of external-id part is:
-    External_IDs
-       External_ID
-          IDtype
-          IDvalue
-    """
-    mod_note = 'Adding LibrisXL ID based on Libris ID (by api)'
-    # get entry to ensure it exists and to get remaining external-ids
-    old_data = get_libris_data(libris_id)
-    # get new id
-    librisxl_id = get_librisxl_id(libris_id)
-    # extract all pre-existing external ids
-    external_id = []
-    for entry in old_data.get('External_IDs').get('External_ID'):
-        if entry.get('IDtype') == LIBRISXL_IDTYPE:
-            raise ValueError('Record already has LibrisXL ID.')
-        external_id.append({
-            'IDtype': entry.get('IDtype'),
-            'IDvalue': entry.get('IDvalue')})
-    # add new id
-    external_id.append({
-        'IDtype': LIBRISXL_IDTYPE,
-        'IDvalue': librisxl_id})
-    # package as update
-    update = {'External_IDs': {'External_ID': external_id}}
-    return update_publication(old_data, update, mod_note)
-
-
-# drop
-def test():
-    add_librisxl_id(7245873)
-    # print(json.dumps(get_libris_data(7245873), indent=4))
-
-
-# drop
-if __name__ == "__main__":
-    test()
-
-
-# SQLGetPubById - the internal mechanism by which pub record could be retieved
+        http://www.isfdb.org/wiki/index.php/XML:PubUpdate
+        """
+        data = {
+            'Record': old_data['Record'],
+            **update
+        }
+        return self.make_submission(
+            'PubUpdate', data, old_data['Title'], mod_note)
